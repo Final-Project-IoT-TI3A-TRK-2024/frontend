78a84efa12996630d238d27d5481bb7727a0ee11
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-11-23 16:25:19 +0700

    chore(footer): add about us

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
-import {Dot} from "lucide-react";
+import {Dot, Heart} from "lucide-react";
 import Stats from "@/app/stats";
+import { CalendarIcon } from "lucide-react"
 
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 export default function Home() {
   return (
-    <div className="grid p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid p-8 pb-20 gap-10 font-[family-name:var(--font-geist-sans)]">
       <div className="items-center justify-items-center">
         <Image
           src="/images/polines.png"
@@ -15,17 +27,32 @@ export default function Home() {
         />
       </div>
       <main className="flex flex-col gap-2">
-        <Stats />
+        <Stats/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Teknologi Rekayasa Komputer<Dot />TI-3A
-        </a>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Button className="flex items-center gap-2 hover:underline hover:underline-offset-4" variant="link">
+              Teknologi Rekayasa Komputer<Dot/>TI-3A
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <div className="space-y-1">
+                <h4 className="text-sm font-semibold">Who are we?</h4>
+                <p className="text-sm">
+                  The IoT Agriculture Team is a group of students from the Politeknik Negeri Semarang who are passionate about agriculture and technology.
+                </p>
+                <div className="flex items-center pt-2">
+                  <Heart className="mr-2 h-4 w-4 opacity-70" fill={"red"}/>{" "}
+                  <span className="text-xs text-muted-foreground">
+                    Made with love.
+              </span>
+                </div>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </footer>
     </div>
   );
